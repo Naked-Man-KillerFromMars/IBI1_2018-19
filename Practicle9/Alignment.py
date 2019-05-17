6473877667ac3700005bd2ebb589d83e2f703f79
Zhu Wenyuan <wenyuan.18@intl.zju.edu.cn>
2019-05-17 11:53:32 +0800

    mistake in Practical9

--- Practicle9/Alignment.py
+++ Practicle9/Alignment.py
@@ -5,7 +5,7 @@
 #@author: 11601
 #"""
 import xlrd 
-blosum62 = xlrd.open_workbook('C:/Users/11601/Desktop/IBI/IBI1_2018-19/Practicle9/BLOSUM62.xlsx')
+blosum62 = xlrd.open_workbook('BLOSUM62.xlsx')
 sheet1 = blosum62.sheet_by_name('Sheet1')
 
 myDict = {'A': 1, 'R': 2, 'N': 3, 'D': 4, 'C': 5, 'Q': 6, 'E': 7, 'G': 8, 'H': 9, 'I': 10, 'L': 11, 'K': 12, 'M': 13, 'F': 14, 'P': 15, 'S': 16, 'T': 17, 'W': 18, 'Y': 19, 'V': 20}
@@ -35,11 +35,12 @@ while len(Temp1) > 0:
 
 print('Comparing human sequence and mice:')
 print('edit distance = ' + str(Diff))
-print('ratio of difference = ' + str(Diff/(Diff + Sim)))
+print('percentage of identity = ' + str((1-Diff/(Diff + Sim))*100)+'%')
 
-print('Score is ' + str(Score))
+print('BLOSUM score = ' + str(Score))
+print('Normalized BLOSUM score (to the sequence length) = ' + str(Score/len(Seq1)))
 
-print('                    ')
+print(' ')
 
 Temp1 = list(Seq1)
 Temp2 = list(Seq2)
@@ -63,11 +64,12 @@ while len(Temp1) > 0:
 
 print('Comparing human sequence and random:')
 print('edit distance = ' + str(Diff))
-print('ratio of difference = ' + str(Diff/(Diff + Sim)))
+print('percentage of identity = ' + str((1-Diff/(Diff + Sim))*100)+'%')
 
-print('Score is ' + str(Score))
+print('BLOSUM score = ' + str(Score))
+print('Normalized BLOSUM score (to the sequence length) = ' + str(Score/len(Seq1)))
 
-print('                    ')
+print('  ')
 
 Temp1 = list(Seq1)
 Temp2 = list(Seq2)
@@ -90,6 +92,8 @@ while len(Temp2) > 0:
 
 print('Comparing mice sequence and random:')
 print('edit distance = ' + str(Diff))
-print('ratio of difference = ' + str(Diff/(Diff + Sim)))
+print('percentage of identity = ' + str((1-Diff/(Diff + Sim))*100)+'%')
 
-print('Score is ' + str(Score))
+
+print('BLOSUM score = ' + str(Score))
+print('Normalized BLOSUM score (to the sequence length) = ' + str(Score/len(Seq1)))
